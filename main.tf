bdbd1519161f5f8e6451d05e5bdbd2833f5c1dc2
kouno <wallthings4u@gmail.com>
2026-04-13 11:42:46 +0900

    subnet作成

--- main.tf
+++ main.tf
@@ -11,3 +11,11 @@ provider "aws" {
    
   }
 
+  // サブネットの作成
+     resource "aws_subnet" "terra_subnet" {
+         vpc_id = aws_vpc.terra_vpc.id
+         cidr_block = "10.0.0.0/24"
+         tags = {
+             Name = "aws_subnet_name"
+         }
+     }
