ed019f29606f31fc267924c32b19a9f003ed0bc3
kouno <wallthings4u@gmail.com>
2026-04-13 11:41:41 +0900

    vpc作成

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-northeast-1"
+ }
+
+  resource "aws_vpc" "terra_vpc" {
+
+     cidr_block = "10.0.0.0/16"
+     tags = {
+         Name = "aws_vpc_handson"
+     }
+   
+  }
+
